25cce290996469a50fd8faddb73da9476bea2395
XUSANADH-at-394203625936 <utkarsh.sanadhya@accenture.com>
2019-04-18 13:04:45 +0530

    Add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ request({url, json: true}, (error, {body}) => {
     }else if (body.error){
         callback('Unable to find location', undefined)
     }else {
-callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        
+callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There high today is ' + body.daily.data[0].temperatureHigh + ' with a low of '+ body.daily.data[0].temperatureHigh +' There is ' + body.currently.precipProbability + '% chance of rain.')
     }
 })
 }
